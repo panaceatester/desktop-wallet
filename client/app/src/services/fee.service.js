c1bda7389859801b0bcaf0297e63c20db2f4d575
Lucio Rubens <luciorubeens@gmail.com>
2018-06-09 11:33:41 -0300

    chore: fetch fees from autoconfigure

--- client/app/src/services/fee.service.js
+++ client/app/src/services/fee.service.js
@@ -2,40 +2,68 @@
   'use strict'
 
   angular.module('arkclient.services')
-    .service('feeService', [FeeService])
+    .service('feeService', ['$q', 'networkService', FeeService])
 
   /**
    * FeeService
    * @constructor
    */
-  function FeeService () {
-
-    // TODO get real data from autoconfig endpoint
-    const data = [
+  function FeeService ($q, networkService) {
+    const defaultFees = [
       {
         type: 0,
-        max: 1000000,
-        min: 0,
-        avg: 400000
-      }, {
+        avg: 10000000,
+        maxFee: 10000000,
+        minFee: 10000000
+      },
+      {
+        type: 1,
+        avg: 500000000,
+        max: 500000000,
+        min: 500000000
+      },
+      {
         type: 2,
-        max: 1000000,
-        min: 0,
-        avg: 400000
-      }, {
+        avg: 2500000000,
+        maxFee: 2500000000,
+        minFee: 2500000000
+      },
+      {
         type: 3,
-        max: 1000000,
-        min: 0,
-        avg: 400000
+        avg: 100000000,
+        maxFee: 100000000,
+        minFee: 100000000
       }
     ]
 
-    const byType = type => {
+    let cachedFees = null
+
+    const byType = async type => {
+      const data = await getFees()
       return data.find(d => d.type === type)
     }
 
+    const getFees = (canUseCached = false) => {
+      const deferred = $q.defer()
+
+      if (canUseCached && cachedFees) {
+        deferred.resolve(cachedFees)
+      }
+
+      networkService.getFromPeer('/api/loader/autoconfigure')
+        .then((resp) => {
+          if (resp.success) {
+            cachedFees = resp.feeStatistics
+            deferred.resolve(cachedFees)
+          } else {
+            deferred.resolve(defaultFees)
+          }
+        }, () => deferred.resolve(defaultFees))
+    }
+
     return {
-      byType
+      byType,
+      getFees
     }
   }
 })()
